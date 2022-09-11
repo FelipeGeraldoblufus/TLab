f33734800e62c5ca8df5bdbcb79f9f143613408d
FelipeGeraldoblufus <107533645+FelipeGeraldoblufus@users.noreply.github.com>
2022-09-11 18:26:18 -0300

    Update Ramos.hpp

---
+++ Nueva carpeta/Ramos.hpp
@@ -0,0 +1,18 @@
+#ifndef RAMOS_H
+#define RAMOS_H
+#include<string>
+using namespace std;
+
+
+class Ramos {
+
+private: 
+    string nombreramo;
+
+public:
+    Ramos(string _nombreramo);
+    string getnombreramo();
+    
+}
+
+#endif
